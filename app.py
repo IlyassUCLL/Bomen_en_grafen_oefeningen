1d1a2835f6ef64be3a883e8714a02a9438ffe252
Yassine-Sayadi <yassine.sayadi@student.ucll.be>
2022-04-19 11:33:05 +0200

    Drawing a Circle and naive animation

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import pygame
+from pygame.display import flip
+
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
 
 
 # Initialize Pygame
@@ -6,16 +10,26 @@ pygame.init()
 
 def create_main_surface():
     # Tuple representing width and height in pixels
-    screen_size = (1024, 768)
+    screen_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
     # Create window with given size
-    pygame.display.set_mode(screen_size)
-def main():
-    create_main_surface()
+    return pygame.display.set_mode(screen_size)
 
+def render_frame(surface, x):
+    pygame.draw.circle(surface, (50,50,50), (x, SCREEN_HEIGHT/2), 100, 0)
+    flip()
+
+
+def main():
+    surface = create_main_surface()
+    
     running = True
     while (running == True):
+
+        render_frame(surface,  pygame.mouse.get_pos()[0])
+
         for e in pygame.event.get():
+            
             if (e.type == pygame.KEYDOWN): 
                 print ("u win")
                 
