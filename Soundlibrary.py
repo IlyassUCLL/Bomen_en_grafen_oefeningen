fe696e2b968c92b43670241c8a04c9e335645010
Brian Schaumburg <brian.schaumburg@outlook.com>
2022-04-19 12:57:12 +0200

    soundlibary

---
+++ Soundlibrary.py
@@ -0,0 +1,14 @@
+import pygame
+class SoundLibrary:
+    explosion = False
+    first = 0
+    def __init__(self, file=str):
+        if file == 'explosion':
+            self.explosion = pygame.mixer.Sound('5.wav'
+            )
+    def uitvoering(self):
+        pygame.init()
+        var = pygame.mixer.Sound.play(self.explosion)
+        while(var.get_busy()):
+            pygame.time.wait(100)
+
