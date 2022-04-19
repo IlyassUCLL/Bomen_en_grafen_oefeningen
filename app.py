3185978aac5a3ba9a295b2ae1a82a00d1f0e3df7
Stoelste <arne.stoel@student.ucll.be>
2022-04-19 10:33:57 +0200

    Added infinite loop

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ import pygame
 # Initialize Pygame
 pygame.init()
 
-# Tuple representing width and height in pixels
-screen_size = (1024, 768)
+def create_main_surface():
+    # Tuple representing width and height in pixels
+    screen_size = (1024, 768)
 
-# Create window with given size
-pygame.display.set_mode(screen_size)
+    # Create window with given size
+    pygame.display.set_mode(screen_size)
+def main():
+    create_main_surface()
+    while (True):
+        a=0
+
+main()
