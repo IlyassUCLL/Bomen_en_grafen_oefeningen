b3cf1daffb4e5b8666f8496af4c6a614426b4979
Stoelste <arne.stoel@student.ucll.be>
2022-04-19 14:36:19 +0200

    Game State

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import pygame
 from pygame.display import flip
-from time import sleep
 
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
+BG_COLOR = (255, 255, 255)
 
 clock = pygame.time.Clock()
 
@@ -17,33 +17,40 @@ def create_main_surface():
     # Create window with given size
     return pygame.display.set_mode(screen_size)
 
-def render_frame(surface, x):
-    pygame.draw.circle(surface, (0,0,0), (x, SCREEN_HEIGHT/2), 100, 0)
+def render_frame(surface, state):
+    clear_surface(surface, BG_COLOR) # render the frame
+    state.render(surface)
     flip()
 
 def clear_surface(surface, color):
     surface.fill(color)
 
+class State:
+    def __init__(self):
+        self.x = 0
+
+    def updateX(self, d_t): 
+        self.x += 20*d_t # update circle position based on its velocity and d_t
+
+    def render(self, surface):  
+        pygame.draw.circle(surface, (0,0,0), (self.x, SCREEN_HEIGHT/2), 100, 10)
 
 
 def main():
     surface = create_main_surface()
 
-    circle_x = 0
+    state = State()
 
     running = True
     while (running == True):
         d_t = clock.tick()/1000 # update the clock and get time since the last call of .tick()
-        clear_surface(surface, (255,255,255))
-
-        circle_x += 20*d_t # update circle position based on its velocity and d_t
-
-        render_frame(surface,  circle_x) # render the frame
 
         for e in pygame.event.get():
             if (e.type == pygame.KEYDOWN): 
                 print ("u win")
             if (e.type == pygame.QUIT):
                 running = False
+        state.updateX(d_t)
+        render_frame(surface, state)    
     
 main()
