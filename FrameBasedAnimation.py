2d807bdd6593d541f5c0e212a39b3e9c2b129814
Yassine-Sayadi <yassine.sayadi@student.ucll.be>
2022-04-20 15:35:52 +0200

    Made explosions work on mouse click and added position to framebasedanimation

--- FrameBasedAnimation.py
+++ FrameBasedAnimation.py
@@ -2,30 +2,31 @@ class FrameBasedAnimation:
     images = None
     seconds = None
 
-    def __init__(self, images=[], secondsPerFrame=int):
+    def __init__(self, images=[], secondsPerFrame=int, pos=(500,500)):
         time_passed = 0
         self.images = images
         self.seconds = secondsPerFrame
+        self.pos = pos
 
     def render(self, surface):
         if self.seconds <= 0.05:
-            surface.blit(self.images[0], (500, 350))
+            surface.blit(self.images[0], self.pos)
         elif self.seconds >= 0.05 and self.seconds <= 0.1:
-            surface.blit(self.images[1], (500, 350))
+            surface.blit(self.images[1], self.pos)
         elif self.seconds >= 0.1 and self.seconds <= 0.2:
-            surface.blit(self.images[2], (500, 350))
+            surface.blit(self.images[2], self.pos)
         elif self.seconds >= 0.2 and self.seconds <= 0.3:
-            surface.blit(self.images[3], (500, 350))
+            surface.blit(self.images[3], self.pos)
         elif self.seconds >= 0.3 and self.seconds <= 0.4:
-            surface.blit(self.images[4], (500, 350))
+            surface.blit(self.images[4], self.pos)
         elif self.seconds >= 0.4 and self.seconds <= 0.5:
-            surface.blit(self.images[5], (500, 350))
+            surface.blit(self.images[5], self.pos)
         elif self.seconds >= 0.5 and self.seconds <= 0.6:
-            surface.blit(self.images[6], (500, 350))
+            surface.blit(self.images[6], self.pos)
         elif self.seconds >= 0.6 and self.seconds <= 0.7:
-            surface.blit(self.images[7], (500, 350))
+            surface.blit(self.images[7], self.pos)
         elif self.seconds >= 0.7 and self.seconds <= 0.8:
-            surface.blit(self.images[8], (500, 350))
+            surface.blit(self.images[8], self.pos)
 
     def update(self, elapsedtime):
         self.seconds += elapsedtime
