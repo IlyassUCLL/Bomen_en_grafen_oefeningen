67ba602371d81af257b75da168c29bb7a3b11658
Yassine-Sayadi <yassine.sayadi@student.ucll.be>
2022-04-20 09:32:06 +0200

    key processor returns normalized direction

--- app.py
+++ app.py
@@ -17,18 +17,26 @@ def create_main_surface():
     # Create window with given size
     return pygame.display.set_mode(screen_size)
 
-def process_key_input(state, d_t, key):
+def process_key_input(key):
+    v = pygame.Vector2()
+
     if key[pygame.K_LEFT]:
-        state.updateX(d_t, -1)
+        v.x += -1
 
     if key[pygame.K_UP]:
-        state.updateY(d_t, 1)    
+        v.y += -1  
 
     if key[pygame.K_RIGHT]:
-        state.updateX(d_t, 1)
+        v.x += 1
 
     if key[pygame.K_DOWN]:
-        state.updateY(d_t, -1)    
+        v.y += 1
+
+    # returns vector 0 length zero if no key presses or normalized direction
+    if (v.length() == 0):
+        return pygame.Vector2(0,0)
+    return v.normalize()
+    
 def render_frame(surface, state):
     clear_surface(surface, BG_COLOR) # render the frame
     state.render(surface)
@@ -42,11 +50,9 @@ class State:
         self.x = 0
         self.y = 0
 
-    def updateX(self, d_t, dir): 
-        self.x += dir*500*d_t # update circle position based on its velocity and d_t
-
-    def updateY(self, d_t, dir): 
-        self.y += -dir*500*d_t # update circle position based on its velocity and d_t    
+    def update(self, d_t, dir): 
+        self.x += dir.x*500*d_t # update circle position based on its velocity and d_t
+        self.y += dir.y*500*d_t # update circle position based on its velocity and d_t
 
     def render(self, surface):  
         pygame.draw.circle(surface, (0,0,0), (self.x, self.y), 100, 10)
@@ -64,7 +70,9 @@ def main():
             if (e.type == pygame.QUIT):
                 running = False
 
-        process_key_input(state, d_t, pygame.key.get_pressed())
+    
+        #process_key_input(state, pygame.key.get_pressed())
+        state.update(d_t, process_key_input(state, pygame.key.get_pressed()))
         render_frame(surface, state)    
     
 main()
